110cc93fa90fc056d6479a4b884ec169e04a8b48
aushakov95 <ushakoan@gmail.com>
2016-05-15 19:53:14 -0700

    added all required features

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -2,7 +2,16 @@ class Book
  attr_accessor :title
 
   def title=(title)
-    @title = title.capitalize
+    title_array = title.split(" ")
+    title_array.each do |word|
+        unless ['a','the','an','in','of','and'].include? word
+            word.capitalize!
+        end
+    end
+    if title_array.size > 0
+        title_array[0].capitalize!
+    end
+    @title = title_array.join(" ")
   end
 
 end
